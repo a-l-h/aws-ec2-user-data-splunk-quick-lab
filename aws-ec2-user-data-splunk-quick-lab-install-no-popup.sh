4643f711a394f05c61d813e01f7158fae568efd0
d2si-spk <42603348+d2si-spk@users.noreply.github.com>
2018-12-06 08:50:18 +0000

    Update aws-ec2-user-data-splunk-quick-lab-install-no-popup.sh

--- aws-ec2-user-data-splunk-quick-lab-install-no-popup.sh
+++ aws-ec2-user-data-splunk-quick-lab-install-no-popup.sh
@@ -1,64 +1,185 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# This is a simple shell script to use as "User Data" when launching an AWS EC2 instance serving as a Splunk quick Lab.
+#
+# It will install Splunk and perform a few configuration steps so that Splunk is accessible straight away without any of the actions usually required after a fresh install.
+#
+# It will also retrieve Splunk Apps and Add-ons from a provided S3 bucket and install them in the dedicated Splunk directory.
+#
+# The goal is to set up a throwable Splunk instance for Lab purposes without any dialog box to interfere.
+#
+# More info on github : https://github.com/d2si-spk/aws-ec2-user-data-splunk-quick-lab-install-no-popup
+
+# Set the script to exit when a command fails
+set -o errexit
+
+# Set the script to produce a failure return code if any command errors between pipelines 
+set -o pipefail
+
+# Set the script to exit when it tries to use undeclared variables
+set -o nounset
+
+# Provide the name of the bucket you want to retrieve Splunk Apps and Add-ons from
+readonly s3_bucket="<s3_bucket>"
+
+# Provide the admin password you want to set
+export password="<password>"
+
+# Set $SPLUNK_HOME variable
+export SPLUNK_HOME="/opt/splunk"
 
 # Download the latest Splunk version
 
-wget -O splunk-latest-linux-x86_64.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=latest&product=splunk&filename=.tgz&wget=true'
+wget --quiet --output-document splunk-latest-linux-x86_64.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=latest&product=splunk&filename=.tgz&wget=true'
 
-# Untar Splunk tgz file in /opt
+echo "Downloaded latest Splunk build"
 
-sudo tar xvzf splunk-latest-linux-x86_64.tgz -C /opt
+# Unpack Splunk tgz file in /opt
+
+tar --extract --gzip --file splunk-latest-linux-x86_64.tgz --directory /opt
+
+echo "Extracted Splunk to /opt/"
 
 # Delete Splunk tgz file
 
-sudo rm -rf splunk-latest-linux-x86_64.tgz
+rm --recursive --force splunk-latest-linux-x86_64.tgz
 
-# Download content from S3 bucket
+echo "Removed Splunk installation source"
 
-sudo aws s3 cp s3://<s3_bucket_name>/ ./ --recursive
+# Customize global bash prompt with color, shortcut for $SPLUNK_HOME/bin directory, and auto-completion script so it can be used by both root and ec2-user users
 
-# Untar downloaded tgz files in /etc/apps
+{
+  echo "export PS1=\"\e[0;32m[\u@\h \W]\$ \e[m\""
+  echo "export SPLUNK_HOME=/opt/splunk"
+  echo "export PATH=$PATH:$SPLUNK_HOME/bin"
+  echo ". $SPLUNK_HOME/share/splunk/cli-command-completion.sh"
+} >> /etc/bashrc
 
-cat *.tgz | sudo tar xvzf - -i -C /opt/splunk/etc/apps
+echo "Configured global bash prompt"
 
-# Customize bash prompt with color, shortcut for $SPLUNK_HOME/bin directory, and auto-completion script
+# Download Splunk Apps and Add-ons from S3 bucket
 
-echo 'export PS1="\e[0;32m[\u@\h \W]\$ \e[m"' >> ~/.bashrc
-echo 'export SPLUNK_HOME=/opt/splunk' >> ~/.bashrc
-echo 'export PATH=$PATH:$SPLUNK_HOME/bin' >> ~/.bashrc
-echo '. $SPLUNK_HOME/share/splunk/cli-command-completion.sh'  >> ~/.bashrc
+aws s3 cp s3://"${s3_bucket}"/ ./ --quiet --recursive --exclude "*" --include "*.tgz"  --include "*.tar.gz" --include "*.spl" || true
 
-source ~/.bashrc
+echo "Downloaded Apps & Add-ons from s3 bucket ${s3_bucket}"
 
-# Set Splunk Web to run on port 80 and block it from checking for newer version
+# Unpack downloaded tgz files in /etc/apps
 
-echo '[settings]' | sudo tee /opt/splunk/etc/system/local/web.conf
-echo 'httpport = 80' | sudo tee --append /opt/splunk/etc/system/local/web.conf
-echo 'updateCheckerBaseURL = 0' | sudo tee --append /opt/splunk/etc/system/local/web.conf
+cat ./*.tgz | tar --extract --gzip --file - --ignore-zeros --directory "${SPLUNK_HOME}"/etc/apps || true
+
+echo "Extracted Apps & Add-ons to ${SPLUNK_HOME}/etc/apps"
+
+# Delete retrieved Apps and Add-ons
+
+rm --recursive --force ./*.tgz ./*.tar.gz ./*.spl
+
+echo "Removed Apps & Add-ons from source directory"
+
+# Create directories for the 'user_data_no_popup_app' that will be configured through the script 
+
+mkdir --parents "${SPLUNK_HOME}"/etc/apps/user_data_no_popup_app/local "${SPLUNK_HOME}"/etc/apps/user_data_no_popup_app/metadata
+
+# Set metadata permissions for the App
+
+{
+  echo "[]"
+  echo "access = read : [ * ], write : [ admin ]"
+  echo "export = system"
+} > "${SPLUNK_HOME}"/etc/apps/user_data_no_popup_app/metadata/local.meta
+
+# Set Splunk to monitor the output of User Data script  and index it in _internal index
+
+{
+  echo "[monitor:///var/log/cloud-init-output.log]" 
+  echo "index = _internal"
+  echo "sourcetype = aws:cloud-init"
+} > "${SPLUNK_HOME}"/etc/apps/user_data_no_popup_app/local/inputs.conf
+
+{
+  echo "[aws:cloud-init]"
+  echo "DATETIME_CONFIG = CURRENT"
+  echo "BREAK_ONLY_BEFORE = Cloud-init"
+} > "${SPLUNK_HOME}"/etc/apps/user_data_no_popup_app/local/props.conf
+
+echo "Configured monitoring for User Data logs"
+
+# Prevent Splunk Web from checking for newer versions
+
+{
+  echo "[settings]"
+  echo "updateCheckerBaseURL = 0"
+} > "${SPLUNK_HOME}"/etc/apps/user_data_no_popup_app/local/web.conf
 
 # Set user preferences to avoid being spammed with system messages
 
-echo '[general]' | sudo tee /opt/splunk/etc/system/local/user-prefs.conf
-echo 'render_version_messages = 0' | sudo tee --append /opt/splunk/etc/system/local/user-prefs.conf
-echo 'dismissedInstrumentationOptInVersion = 3'  | sudo tee --append /opt/splunk/etc/system/local/user-prefs.conf
-echo 'hideInstrumentationOptInModal = 1' | sudo tee --append /opt/splunk/etc/system/local/user-prefs.conf
+{
+  echo "[general]"
+  echo "render_version_messages = 0"
+  echo "dismissedInstrumentationOptInVersion = 3" 
+  echo "hideInstrumentationOptInModal = 1"
+} > "${SPLUNK_HOME}"/etc/apps/user_data_no_popup_app/local/user-prefs.conf
 
 # Configure password policy to allow a password shorter than the default minimum of 8 characters
 
-echo '[splunk_auth]' | sudo tee /opt/splunk/etc/system/local/authentication.conf
-echo 'minPasswordLength = 1' | sudo tee --append /opt/splunk/etc/system/local/authentication.conf
+{
+  echo "[splunk_auth]"
+  echo "minPasswordLength = 1"
+} > "${SPLUNK_HOME}"/etc/apps/user_data_no_popup_app/local/authentication.conf
+
+# Set a lower limit on disk usage
+
+{
+  echo "[diskUsage]"
+  echo "minFreeSpace = 2000"
+} > "${SPLUNK_HOME}"/etc/apps/user_data_no_popup_app/local/server.conf
+
+# Configure any ui-tour as already viewed to avoid popups
+
+{
+  echo "[default]"
+  echo "viewed = 1"
+} > "${SPLUNK_HOME}"/etc/apps/user_data_no_popup_app/local/ui-tour.conf
+
+echo "Set some configurations to avoid popups"
+
+# Redirect Splunk Web port 8000 to port 80 to reach it only with IP address through Web browser
+
+iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8000
+
+echo "Redirected port 8000 to port 80"
 
 # Fake a previous login to prevent Splunk from requesting password change
 
-sudo touch /opt/splunk/etc/.ui_login
+touch "${SPLUNK_HOME}"/etc/.ui_login
 
-# Start Splunk and set a new password
+echo "Faked a previous UI login"
 
-sudo /opt/splunk/bin/splunk start --accept-license --answer-yes --no-prompt --seed-passwd <password>
+# Set Splunk to start the service as the user 'ec2-user' 
+
+echo "SPLUNK_OS_USER=ec2-user" >> "${SPLUNK_HOME}"/etc/splunk-launch.conf
+
+echo "Set ec2-user as the user to start the Splunk service with"
+
+# Change the ownership of the Splunk directory to the ec2-user
+
+chown -R ec2-user:ec2-user "${SPLUNK_HOME}"
+
+echo "Changed ${SPLUNK_HOME} ownership to ec2-user"
+
+# Start Splunk, accept license and set a admin password
+
+sudo -E -u ec2-user bash -c '${SPLUNK_HOME}/bin/splunk start --accept-license --answer-yes --no-prompt --seed-passwd ${password}'
+
+echo "Set Splunk admin password"
+
+echo "Accepted Splunk license"
+
+echo "Started Splunk"
 
 # Configure Splunk to start at boot time
 
-sudo /opt/splunk/bin/splunk enable boot-start
+"${SPLUNK_HOME}"/bin/splunk enable boot-start -user ec2-user
 
-# Update the system
+echo "Configured Splunk to start at boot time"
 
-sudo yum update -y
+echo "Done"
